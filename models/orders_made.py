232e1acb7013114fc00ae536ba8f9aa16319b72f
Martin Quinn <thefonz270@hotmail.com>
2021-11-03 16:41:24 +0000

    changed date variable type and added css.

--- models/orders_made.py
+++ models/orders_made.py
@@ -1,3 +1,4 @@
+import datetime
 from models.films import Film
 from models.orders import Order
 
@@ -7,7 +8,7 @@ film3 = Film("The Return of the King", 10, 179)
 film4 = Film("Home Alone", 5, 103)
 film5 = Film("Elf", 5, 97)
 
-order1 = Order("Phil", "02/11/21", [film1, film2, film3])
-order2 = Order("Nick", "24/12/21", [film4, film5])
+order1 = Order("Phil", datetime.date(2021, 11, 2), [film1, film2, film3])
+order2 = Order("Nick", datetime.date(2021, 12, 24), [film4, film5])
 
 orders_list = [order1, order2]
