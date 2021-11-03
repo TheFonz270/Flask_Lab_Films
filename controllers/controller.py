cff7aace79bbd404a6a6a8b16f3640fbefae0f54
Martin Quinn <thefonz270@hotmail.com>
2021-11-03 16:01:31 +0000

    Added link to each order in order list to individual order page

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,6 +1,12 @@
 from app import app
 from flask import render_template
+from models.orders_made import orders_list
 
 @app.route('/')
 def index():
-    return 'Hello, world!'
+    return render_template('index.html', orders_list=orders_list)
+
+@app.route('/order/<i>')
+def order(i):
+    n=int(i)
+    return render_template('order.html', order=orders_list[n])
